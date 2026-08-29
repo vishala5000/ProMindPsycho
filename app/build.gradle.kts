9c92719c64b5471167ec5dc098ea516eb2358e71
Simple <157860608+vishala5000@users.noreply.github.com>
2026-08-29 07:58:32 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,25 +4,51 @@ plugins {
 }
 
 android {
-    namespace = "com.example.quotevideogenerator"
+    namespace = "com.quotegenerator"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.quotevideogenerator"
-        minSdk = 24
+        applicationId = "com.quotegenerator"
+        minSdk = 23
         targetSdk = 35
+
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
         }
+
+        debug {
+            isMinifyEnabled = false
+        }
     }
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.7.1")
-    implementation("dev.ffmpegkit-maintained:ffmpeg-kit-full-gpl:8.1.7")
+
+    implementation("androidx.core:core-ktx:1.15.0")
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("com.google.android.material:material:1.12.0")
+
+    implementation("androidx.activity:activity-ktx:1.10.1")
 }
